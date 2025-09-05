ce61cdf42d8eeed722a2ccdcd3806f45587f2e1a
Yuan Ma <mayuan131522@gmail.com>
2025-09-05 21:01:29 +0000

    main

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,34 +7,48 @@ import { useEffect, useState } from "react";
 
 //create your first component
 const Home =() =>{
-  const [name,setName] = useState("")
+ const [name, setName] = useState("");
+ const [email, setEmail] = useState("");
+ const [country, setCountry] = useState("");
+ const [picture, setPicture] = useState("");
 
  useEffect(() => {
-  const fetchData = async() =>{
-try {
-  const response = await fetch("https://randomuser.me/api");
-  if (!response.ok) {
-    console.log("there is an error");
-    return;
-  }
-  const data = await response.json();
-  console.log(data);
-  
-  
-} catch (error) {
-  throw new Error("something wrong with fetch");
-  
-}  
-  }
-  fetchData();
-}
- ,[])
+   
+ const fetchData = async() => {
+  try {
+     const response = await fetch("https://randomuser.me/api");
+     if (!response.ok) {
+        console.log("there is an error");
+     }
+     const data = await response.json();
+     console.log(data);
+     const fullName = `${data.results[0].name.first} ${data.results[0].name.last}`;
+    //  const email = data.result[0].email;
+     console.log(email);
+     setName(fullName);
+     setEmail(data.results[0].email)
 
- return (
-  <div>
 
-  </div>
- )
+  } catch (error) {
+    throw new Error("something is wrong with fetch");
+    
+  }
+ }
+
+
+fetchData();
+
+ }, [])
+ 
+
+
+return (
+ <div>
+    <h1>{name}</h1>
+ <p>{email}</p>
+ </div>
+)
+ 
 }
 
 export default Home;
